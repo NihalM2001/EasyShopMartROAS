0fd21e4bc7f4e8161df7823de1830b170a6bfe35
nihal <nihalmurali047@gmail.com>
2024-05-04 12:57:45 +0530

    Finished calculating all the values from Calculated Values section.

--- script.js
+++ script.js
@@ -6,24 +6,37 @@ document.addEventListener("DOMContentLoaded", function () {
     //inputting all the values
 
     var productCost = document.getElementById("productCostInput").value;
-    console.log(productCost);
+    // console.log(productCost);
     var packagingCost = document.getElementById("packagingCostInput").value;
-    console.log(packagingCost);
+    // console.log(packagingCost);
     var shippingCost = document.getElementById("shippingCostInput").value;
     console.log(shippingCost);
     var productSP = document.getElementById("productSPInput").value;
-    console.log(productSP);
+    // console.log(productSP);
     var adSpend = document.getElementById("adSpendInput").value;
-    console.log(adSpend);
+    // console.log(adSpend);
     var expectedMargin = document.getElementById("expectedMarginInput").value;
-    console.log(expectedMargin);
-    var rto = document.getElementById("rtoInput").value;
-    console.log(rto);
-    var damagedReturn = document.getElementById("damagedReturnInput").value;
-    console.log(damagedReturn);
+    // console.log(expectedMargin);
+    var rto = parseInt(document.getElementById("rtoInput").value);
+    // console.log(rto);
+    var damagedReturn = parseInt(
+      document.getElementById("damagedReturnInput").value
+    );
+    // console.log(damagedReturn);
     var reuseInventory = document.getElementById("reuseInventoryInput").value;
-    console.log(reuseInventory);
+    // console.log(reuseInventory);
 
     //calculating all the required fields
+    var s2pCostMultiplier = productSP / productCost;
+    // console.log(s2pCostMultiplier);
+
+    var deliOrderPercent = 100 - (rto + damagedReturn);
+    // console.log(deliOrderPercent);
+
+    var returnShippingCost = shippingCost * 0.75;
+    console.log(returnShippingCost);
+
+    var rto = shippingCost * 0.75;
+    console.log(rto);
   });
 });
