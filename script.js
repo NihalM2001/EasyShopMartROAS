f453c60f0a896b17c7aabfd72a6bfd06f67a6fc9
nihal <nihalmurali047@gmail.com>
2024-05-16 21:42:13 +0530

    Finished calculating the formulas for other Costs. over to calculating values for the breakeven section

--- script.js
+++ script.js
@@ -5,38 +5,92 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //inputting all the values
 
-    var productCost = document.getElementById("productCostInput").value;
-    // console.log(productCost);
+    var productCost = parseInt(
+      document.getElementById("productCostInput").value
+    );
+    console.log("productCost " + productCost);
     var packagingCost = document.getElementById("packagingCostInput").value;
     // console.log(packagingCost);
     var shippingCost = document.getElementById("shippingCostInput").value;
-    console.log(shippingCost);
+    console.log("shippingCost " + shippingCost);
     var productSP = document.getElementById("productSPInput").value;
     // console.log(productSP);
     var adSpend = document.getElementById("adSpendInput").value;
     // console.log(adSpend);
     var expectedMargin = document.getElementById("expectedMarginInput").value;
     // console.log(expectedMargin);
-    var rto = parseInt(document.getElementById("rtoInput").value);
-    // console.log(rto);
-    var damagedReturn = parseInt(
+    var rto = parseFloat(document.getElementById("rtoInput").value);
+    console.log("rto% " + rto);
+    var damagedReturn = parseFloat(
       document.getElementById("damagedReturnInput").value
     );
-    // console.log(damagedReturn);
+    console.log("damagedReturn " + damagedReturn);
     var reuseInventory = document.getElementById("reuseInventoryInput").value;
     // console.log(reuseInventory);
 
     //calculating all the required fields
+
+    /*
+    Calculated Values Section
+    */
+
     var s2pCostMultiplier = productSP / productCost;
     // console.log(s2pCostMultiplier);
 
     var deliOrderPercent = 100 - (rto + damagedReturn);
     // console.log(deliOrderPercent);
 
-    var returnShippingCost = shippingCost * 0.75;
-    console.log(returnShippingCost);
+    var returnShippingCost = shippingCost * 0.8;
+    // console.log(returnShippingCost);
 
-    var rto = shippingCost * 0.75;
-    console.log(rto);
+    var rtoCost = shippingCost * 0.8;
+    // console.log(rtoCost);
+
+    /*
+    
+    */
+
+    var revenueDelivered = productSP * (deliOrderPercent / 100);
+    // console.log(revenueDelivered);
+
+    var freightCostRTO = (rto / 100) * rtoCost;
+    console.log("freightCostRTO " + freightCostRTO);
+
+    var freightCostDamaged = (damagedReturn / 100) * shippingCost;
+    console.log("freightCostDamaged " + freightCostDamaged);
+
+    var totalFreightCharge =
+      parseFloat(shippingCost) +
+      parseFloat(freightCostRTO) +
+      parseFloat(freightCostDamaged);
+
+    console.log("totalFreightCharge " + totalFreightCharge);
+
+    var repackingCharges = () => {
+      if (reuseInventory == 0) {
+        return 0;
+      } else {
+        return (rto / 100) * packagingCost;
+      }
+    };
+
+    console.log("repackingCharges " + repackingCharges());
+
+    var rtoInventorySavings = () => {
+      if (reuseInventory == 0) {
+        return 0;
+      } else {
+        return (rto / 100) * productCost;
+      }
+    };
+
+    console.log("rtoInventorySavings " + rtoInventorySavings());
+
+    var totalNonMarketingCost =
+      productCost +
+      totalFreightCharge -
+      rtoInventorySavings() +
+      repackingCharges();
+    console.log("totalNonMarketingCost " + totalNonMarketingCost);
   });
 });
